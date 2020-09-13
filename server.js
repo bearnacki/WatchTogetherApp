22f6f5095ed18084ebfe55d9efce545842d89fbb
Patryk Biernat <bearnacki@gmail.com>
2020-09-13 13:42:40 +0200

    npm audit fix

--- server.js
+++ server.js
@@ -1,32 +1,32 @@
-const express = require('express');
-const socket = require('socket.io');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const db = require('knex')({
-  client: 'pg',
+const express = require("express");
+const socket = require("socket.io");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const db = require("knex")({
+  client: "pg",
   connection: {
-    host: '127.0.0.1',
-    user: 'postgres',
-    password: '',
-    database: 'watchapp'
-  }
+    host: "127.0.0.1",
+    user: "postgres",
+    password: "",
+    database: "watchapp",
+  },
 });
 
 const app = express();
 
 const server = app.listen(process.env.PORT || 3000, () => {
-  console.log('Knock, knock? Can I go now?');
+  console.log("Knock, knock? Can I go now?");
 });
 
 const io = socket(server);
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
 app.use(bodyParser.json());
 
 app.use(cors());
 
-app.post('/register', (req, res) => {
+app.post("/register", (req, res) => {
   const { url } = req.body;
   //to do: validating url
   const id =
@@ -38,44 +38,44 @@ app.post('/register', (req, res) => {
       .substring(2, 15);
   db.insert({
     hash: id,
-    url: url
+    url: url,
   })
-    .into('chats')
-    .returning('hash')
-    .then(hash => {
+    .into("chats")
+    .returning("hash")
+    .then((hash) => {
       console.log(hash);
       res.status(200).json(hash[0]);
     })
-    .catch(err => {
-      res.status(400).send('Wrong link');
+    .catch((err) => {
+      res.status(400).send("Wrong link");
     });
 });
 
-app.get('/link/:id', (req, res) => {
+app.get("/link/:id", (req, res) => {
   const { id } = req.params;
-  db.select('url')
-    .from('chats')
-    .where('hash', id)
-    .then(response => {
+  db.select("url")
+    .from("chats")
+    .where("hash", id)
+    .then((response) => {
       if (response[0] == undefined) {
-        res.status(404).send('Not found');
+        res.status(404).send("Not found");
       } else {
-        res.render('link.ejs', response[0]);
+        res.render("link.ejs", response[0]);
       }
     });
 });
 
-io.on('connection', socket => {
-  socket.on('subscribe', roomId => {
+io.on("connection", (socket) => {
+  socket.on("subscribe", (roomId) => {
     socket.join(roomId);
   });
-  socket.on('chat', data => {
-    io.sockets.to(data.roomId).emit('chat', data);
+  socket.on("chat", (data) => {
+    io.sockets.to(data.roomId).emit("chat", data);
   });
-  socket.on('typing', data => {
-    socket.broadcast.to(data.roomId).emit('typing', data);
+  socket.on("typing", (data) => {
+    socket.broadcast.to(data.roomId).emit("typing", data);
   });
-  socket.on('playing', data => {
-    socket.broadcast.to(data.roomId).emit('playing', data);
+  socket.on("playing", (data) => {
+    socket.broadcast.to(data.roomId).emit("playing", data);
   });
 });
